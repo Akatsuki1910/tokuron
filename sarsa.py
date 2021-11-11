e3c0a3303870767fee4b0bd6062b05758d0f3faf
akatsuki1910 <akatsukicirno@gmail.com>
2021-11-11 21:51:42 +0900

    Update sarsa.py

--- sarsa.py
+++ sarsa.py
@@ -10,27 +10,30 @@ GAMMA = 0.9
 ALPHA = 0.1
 
 
-def action_select(s_s):
+def action_select(s_s, epsilon):
     """ action select """
-    return np.random.choice([i for i in range(1, 4) if i + s_s < 11])
+    arr = [i for i in range(1, 4) if i + s_s < 11]
+
+    if np.random.randint(0, 100) < 10:
+        epsilon = 1
+
+    return np.argmax(Q[s_s, :max(arr)])+1 if epsilon == 0 else np.random.choice(arr)
 
 
 for i in range(10000):
     S_STATE = 0
-    a_state = action_select(S_STATE)
+    a_state = action_select(S_STATE, 0)
     while S_STATE != 10:
         R = 0.001
         s_state_dash = S_STATE + a_state
         if s_state_dash == 10:
             R = -10
-            a_state_dash = 1
         else:
-            s_state_dash = action_select(s_state_dash) + s_state_dash
+            s_state_dash = action_select(s_state_dash, 1) + s_state_dash
             if s_state_dash == 10:
                 R = 10
-                a_state_dash = 1
             else:
-                a_state_dash = action_select(s_state_dash)
+                a_state_dash = action_select(s_state_dash, 0)
 
         Q[S_STATE, a_state-1] = Q[S_STATE, a_state-1]+ALPHA * \
             (R+GAMMA * Q[s_state_dash, a_state_dash-1] - Q[S_STATE, a_state-1])
@@ -38,5 +41,4 @@ for i in range(10000):
         S_STATE = s_state_dash
         a_state = a_state_dash
 
-print(Q)
 plot.plot_func(Q)
