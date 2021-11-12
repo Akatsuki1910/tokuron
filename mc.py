c853bafaf5e48d6dd4f930bea3fb5932e65b52db
Akatsuki1910 <akatsukicirno@gmail.com>
2021-11-12 16:38:33 +0900

    Update mc.py

--- mc.py
+++ mc.py
@@ -7,9 +7,14 @@ import plot
 Q = np.array(np.zeros([11, 3]))
 
 
-def action_select(s_s):
+def action_select(s_s, epsilon):
     """ action select """
-    return np.random.choice([i for i in range(1, 4) if i + s_s < 11])
+    arr = [i for i in range(1, 4) if i + s_s < 11]
+
+    if np.random.randint(0, 100) < 10:
+        epsilon = 1
+
+    return np.argmax(Q[s_s, :max(arr)])+1 if epsilon == 0 else np.random.choice(arr)
 
 
 sr = np.array(np.zeros([11, 3]))
@@ -19,21 +24,21 @@ for i in range(10000):
     EPI = 0
     memory = []
     while S_STATE != 10:
-        a_state = action_select(S_STATE)
-        R = 0.001
+        a_state = action_select(S_STATE, 0)
+        R = 0
         memory.append([S_STATE, a_state])
 
         s_state_dash = S_STATE + a_state
         if s_state_dash == 10:
             R = -10
         else:
-            s_state_dash = action_select(s_state_dash)+s_state_dash
+            s_state_dash = action_select(s_state_dash, 1)+s_state_dash
             if s_state_dash == 10:
                 R = 10
 
         EPI += 1
 
-        if R != 0.001:
+        if R != 0:
             for l in range(EPI):
                 st = memory[l][0]
                 at = memory[l][1]-1
@@ -45,5 +50,4 @@ for i in range(10000):
 
         S_STATE = s_state_dash
 
-print(Q)
 plot.plot_func(Q)
