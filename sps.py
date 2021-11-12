1a97cf34a379983ba2ed0e68b3959d4adc719925
Akatsuki1910 <akatsukicirno@gmail.com>
2021-11-12 16:43:07 +0900

    Update sps.py

--- sps.py
+++ sps.py
@@ -6,12 +6,17 @@ import plot
 
 Q = np.array(np.zeros([11, 3]))
 
-C_BID = 0.1
+C_BID = 0.5
 
 
-def action_select(s_s):
+def action_select(s_s, epsilon):
     """ action select """
-    return np.random.choice([i for i in range(1, 4) if i + s_s < 11])
+    arr = [i for i in range(1, 4) if i + s_s < 11]
+
+    if np.random.randint(0, 100) < 10:
+        epsilon = 1
+
+    return np.argmax(Q[s_s, :max(arr)])+1 if epsilon == 0 else np.random.choice(arr)
 
 
 for i in range(10000):
@@ -19,14 +24,14 @@ for i in range(10000):
     EPI = 0
     memory = []
     while S_STATE != 10:
-        a_state = action_select(S_STATE)
-        R = 0.1
+        a_state = action_select(S_STATE, 0)
+        R = 0.001
 
         s_state_dash = S_STATE + a_state
         if s_state_dash == 10:
             R = -10
         else:
-            s_state_dash = action_select(s_state_dash)+s_state_dash
+            s_state_dash = action_select(s_state_dash, 1)+s_state_dash
             if s_state_dash == 10:
                 R = 10
 
@@ -37,10 +42,10 @@ for i in range(10000):
             for l in range(EPI):
                 st = memory[l][0]
                 at = memory[l][1]-1
-                Q[st, at] += C_BID*(memory[l][2]-Q[st, at])
+                Q[st, at] += C_BID*(R-Q[st, at])
             EPI = 0
             memory = []
 
-        S_STATE += a_state
+        S_STATE = s_state_dash
 
 plot.plot_func(Q)
